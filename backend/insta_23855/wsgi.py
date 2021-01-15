8467611ac0b6fe3d87b04fd67d3d73cb5c53c2b6
Crowdbotics <team@crowdbotics.com>
2021-01-15 02:35:53 +0000

    Update models

--- backend/insta_23855/wsgi.py
+++ backend/insta_23855/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insta_23855.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "insta_23855.settings")
 
 application = get_wsgi_application()
