72b7b1d99d8ae36e87219401661e2a7f2ad29768
Crowdbotics <team@crowdbotics.com>
2021-01-15 16:53:48 +0000

    add package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'UserProfile193544', human_name: 'User Profile', access_route: 'UserProfile193544'},
 { name: 'CalendarView3193369', human_name: 'Calendar View 3', access_route: 'CalendarView3193369', icon: 'calendar'},
 
   // you can add more installed blueprints here
